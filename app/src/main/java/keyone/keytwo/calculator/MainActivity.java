1cea845b20f38f73e494d4a7097a7dfc3383933a
MEA <mayer_evgenija@mail.ru>
2021-07-30 14:41:16 +0700

    Написала обработку каждой кнопки калькулятора
    Добавила метод initListeners для слушателей всех кнопок, метод onToast для вывода сообщений по некоторым кнопкам. В макете сделала TextView некликабельным.

--- app/src/main/java/keyone/keytwo/calculator/MainActivity.java
+++ app/src/main/java/keyone/keytwo/calculator/MainActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -36,7 +37,29 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         initView();
+        initListeners();
+    }
 
+    private void initListeners() {
+        buttonC.setOnClickListener(this);
+        buttonEraseToTheLeft.setOnClickListener(this);
+        buttonDivision.setOnClickListener(this);
+        button1.setOnClickListener(this);
+        button2.setOnClickListener(this);
+        button3.setOnClickListener(this);
+        buttonMultiply.setOnClickListener(this);
+        button4.setOnClickListener(this);
+        button5.setOnClickListener(this);
+        button6.setOnClickListener(this);
+        buttonMinus.setOnClickListener(this);
+        button7.setOnClickListener(this);
+        button8.setOnClickListener(this);
+        button9.setOnClickListener(this);
+        buttonPlus.setOnClickListener(this);
+        buttonPercent.setOnClickListener(this);
+        button0.setOnClickListener(this);
+        buttonComma.setOnClickListener(this);
+        buttonEqual.setOnClickListener(this);
     }
 
     private void initView() {
@@ -62,14 +85,69 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         buttonEqual = findViewById(R.id.buttonEqual);
     }
 
+    public void onToast(String message) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
+    }
+
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
             case R.id.buttonC:
-                //
+                inputField.setText("");
                 break;
             case R.id.buttonEraseToTheLeft:
-                //
+                onToast("EraseToTheLeft");
+                break;
+            case R.id.buttonDivision:
+                inputField.setText(String.format("%s%s", inputField.getText(), "/"));
+                break;
+            case R.id.button1:
+                inputField.setText(String.format("%s%s", inputField.getText(), button1.getText()));
+                break;
+            case R.id.button2:
+                inputField.setText(String.format("%s%s", inputField.getText(), button2.getText()));
+                break;
+            case R.id.button3:
+                inputField.setText(String.format("%s%s", inputField.getText(), button3.getText()));
+                break;
+            case R.id.buttonMultiply:
+                inputField.setText(String.format("%s%s", inputField.getText(), "*"));
+                break;
+            case R.id.button4:
+                inputField.setText(String.format("%s%s", inputField.getText(), button4.getText()));
+                break;
+            case R.id.button5:
+                inputField.setText(String.format("%s%s", inputField.getText(), button5.getText()));
+                break;
+            case R.id.button6:
+                inputField.setText(String.format("%s%s", inputField.getText(), button6.getText()));
+                break;
+            case R.id.buttonMinus:
+                inputField.setText(String.format("%s%s", inputField.getText(), "-"));
+                break;
+            case R.id.button7:
+                inputField.setText(String.format("%s%s", inputField.getText(), button7.getText()));
+                break;
+            case R.id.button8:
+                inputField.setText(String.format("%s%s", inputField.getText(), button8.getText()));
+                break;
+            case R.id.button9:
+                inputField.setText(String.format("%s%s", inputField.getText(), button9.getText()));
+                break;
+            case R.id.buttonPlus:
+                inputField.setText(String.format("%s%s", inputField.getText(), "+"));
+                break;
+            case R.id.buttonPercent:
+                onToast("Percent");
+                break;
+            case R.id.button0:
+                inputField.setText(String.format("%s%s", inputField.getText(), button0.getText()));
+                break;
+            case R.id.buttonComma:
+                inputField.setText(String.format("%s%s", inputField.getText(), ","));
+                break;
+            case R.id.buttonEqual:
+                onToast("Equal");
                 break;
             default:
                 break;
