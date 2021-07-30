7a20999967a8cb6fcbf872ea559f9edac4a3a897
MEA <mayer_evgenija@mail.ru>
2021-07-30 20:49:30 +0700

    В классе Calculator имплиментировала интерфейс Parcelable
    переопределила его методы, интерфейс; добавила в класс конструктор.

--- app/src/main/java/keyone/keytwo/calculator/Calculator.java
+++ app/src/main/java/keyone/keytwo/calculator/Calculator.java
@@ -1,10 +1,34 @@
 package keyone.keytwo.calculator;
 
-public class Calculator {
+import android.os.Parcel;
+import android.os.Parcelable;
+
+public class Calculator implements Parcelable {
 
     private String mathSymbol = "";
     private int number = 0;
     private int result;
+
+    Calculator() {
+    }
+
+    protected Calculator(Parcel in) {
+        mathSymbol = in.readString();
+        number = in.readInt();
+        result = in.readInt();
+    }
+
+    public static final Creator<Calculator> CREATOR = new Creator<Calculator>() {
+        @Override
+        public Calculator createFromParcel(Parcel in) {
+            return new Calculator(in);
+        }
+
+        @Override
+        public Calculator[] newArray(int size) {
+            return new Calculator[size];
+        }
+    };
 
     public void setMathSymbol(String mathSymbol) {
         this.mathSymbol = mathSymbol;
@@ -20,5 +44,17 @@ public class Calculator {
             result = number * number; // просто чтобы было что-нибудь, работает некорректно
         }
         return result;
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(mathSymbol);
+        dest.writeInt(number);
+        dest.writeInt(result);
     }
 }
