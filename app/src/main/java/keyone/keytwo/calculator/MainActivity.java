f04a8f3c4035ab29365d46b9f0309953224ddf0e
MEA <mayer_evgenija@mail.ru>
2021-08-13 19:01:31 +0700

    Добавила кнопку Выбор темы в макет activity_main
    На кнопку повешала Listener, в него поместила интент, чтобы открывалась активити с настройками - приложение падает с ошибкой

--- app/src/main/java/keyone/keytwo/calculator/MainActivity.java
+++ app/src/main/java/keyone/keytwo/calculator/MainActivity.java
@@ -1,5 +1,6 @@
 package keyone.keytwo.calculator;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -13,6 +14,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     static String TAG = "calculator";
 
     TextView inputField;
+    Button buttonSelectTheme;
     Button buttonC;
     Button buttonEraseToTheLeft;
     Button buttonDivision;
@@ -42,6 +44,13 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         initView();
         initListeners();
         calculator = new Calculator();
+        buttonSelectTheme.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this, SettingsActivity.class);
+                startActivity(i);
+            }
+        });
     }
 
     @Override
@@ -85,6 +94,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     private void initView() {
         inputField = findViewById(R.id.inputField);
+        buttonSelectTheme = findViewById(R.id.buttonSelectTheme);
         buttonC = findViewById(R.id.buttonC);
         buttonEraseToTheLeft = findViewById(R.id.buttonEraseToTheLeft);
         buttonDivision = findViewById(R.id.buttonDivision);
